65fb61d6d60f0515d1fc88a29ea7ba61f7a9a0c3
takuyafurugori0602 <kaihatsu.takuya.furugori@gmail.com>
2023-01-27 09:40:41 +0900

    Java課題実装問題1

---
+++ Issue1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Java課題実装1
+ */
+class Issue1 {
+  public static void main(String[] args) {
+
+    // pathの指定を気を付ける
+    // Path.getの中身には開いている階層の下を記載する。
+    try (BufferedReader reader = Files.newBufferedReader(
+        Paths.get("ip001", "scores.csv"))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
